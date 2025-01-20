5ed0523ae00e69486aa7873abf8a5794994b8829
pjpuzzler <pjpuzzler@gmail.com>
2025-01-20 12:38:52 -0500

    Refactor Sphinx configuration for consistency and clarity in project settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,26 +3,30 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'python-balatro'
-copyright = '2025, Patrick J'
-author = 'Patrick J'
-release = '1.0.0'
+project = "python-balatro"
+copyright = "2025, Patrick J"
+author = "Patrick J"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
