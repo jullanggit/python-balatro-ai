56b53bc6a20dd3b1bc49f6a53e42900893f3a1a2
Jul Lang <jullanggit@proton.me>
2025-06-04 15:54:22 +0200

    make Tensors 2D where it makes sense

--- ai/encode.py
+++ ai/encode.py
@@ -1,12 +1,14 @@
+from typing import Dict
 import torch
 from balatro import *
+from typing import Type, List
 
-max_consumables = 20
-max_deck_cards = 100
-max_jokers = 15
-max_hand_cards = 20
+MAX_CONSUMABLES = 20
+MAX_DECK_CARDS = 100
+MAX_JOKERS = 15
+MAX_HAND_CARDS = 20
 
-def enum_to_index(enum_class):
+def enum_to_index(enum_class) -> Dict:
     return {item: idx for idx, item in enumerate(enum_class)}
 
 card_to_index = {**enum_to_index(Tarot), **enum_to_index(Planet), **enum_to_index(Spectral)}
@@ -16,7 +18,7 @@ def one_hot(index: int, num_classes: int) -> torch.FloatTensor:
 
 def encode_ante_tags(ante_tags: list[tuple[Tag, PokerHand | None]]) -> torch.FloatTensor:
     encoded = []
-    for i in range(0, 2):
+    for i in range(2):
         tag, hand = ante_tags[i]
         tag_encoded = one_hot(enum_to_index(Tag)[tag], len(Tag))
 
@@ -25,9 +27,9 @@ def encode_ante_tags(ante_tags: list[tuple[Tag, PokerHand | None]]) -> torch.Flo
         else:
             hand_encoded = one_hot(enum_to_index(PokerHand)[hand], len(PokerHand))
 
-        encoded.append(torch.cat([tag_encoded, hand_encoded]))
+        encoded.append(torch.cat([tag_encoded, hand_encoded]).unsqueeze(0))
 
-    return torch.cat(encoded, dim=0)
+    return torch.cat(encoded)
 
 def encode_blind(blind: Blind) -> torch.FloatTensor:
     return one_hot(enum_to_index(Blind)[blind], len(Blind))
@@ -39,33 +41,33 @@ def encode_bool(bool: bool) -> torch.FloatTensor:
 
 def encode_consumables(consumables: list[Consumable]) -> torch.FloatTensor:
     encoded = []
-    for i in range(0, max_consumables):
+    for i in range(0, MAX_CONSUMABLES):
         if i < len(consumables):
             consumable = consumables[i]
 
             card = one_hot(card_to_index[consumable.card], len(card_to_index))
             is_negative = encode_bool(consumable.is_negative)
 
-            encoded.append(card)
-            encoded.append(is_negative)
+            features = torch.cat(card, is_negative)
+            encoded.append(features.unsqueeze(0))
         else:
-            encoded.append(torch.zeros(len(card_to_index) + 1))
-    return torch.cat(encoded, dim=0)
+            encoded.append(torch.zeros(1, len(card_to_index) + 1))
+    return torch.cat(encoded)
 
 def encode_cards(cards: list[Card], max) -> torch.Tensor:
     """
-    tensor shape: [max*(len(Rank)+len(Suit)+len(Enhancement)+len(Seal)+len(Edition)+3),]
+    tensor shape: [max, (len(Rank)+len(Suit)+len(Enhancement)+len(Seal)+len(Edition)+3)]
     """
 
     encoded = []
-    for i in (0, max):
+    for i in range(max):
         if i < len(cards):
             card = cards[i]
             encoded.append(encode_card(card))
         else:
-            encoded.append(torch.zeros(len(Rank)+len(Suit)+len(Enhancement)+len(Seal)+len(Edition)+3))
+            encoded.append(torch.zeros(1, len(Rank)+len(Suit)+len(Enhancement)+len(Seal)+len(Edition)+3))
 
-    return torch.cat(encoded, dim=0)
+    return torch.cat(encoded)
 
 
 def encode_card(card: Card) -> torch.Tensor:
@@ -73,8 +75,8 @@ def encode_card(card: Card) -> torch.Tensor:
     tensor shape: [len(Rank)+len(Suit)+len(Enhancement)+len(Seal)+len(Edition)+3,]
     """
 
-    rank = one_hot(enum_to_index(Rank)[card.rank])
-    suit = one_hot(enum_to_index(Suit)[card.suit])
+    rank = one_hot(enum_to_index(Rank)[card.rank], len(Rank))
+    suit = one_hot(enum_to_index(Suit)[card.suit], len(Suit))
 
     enhancement_to_index = enum_to_index(Enhancement)
     if card.enhancement is None:
@@ -93,7 +95,8 @@ def encode_card(card: Card) -> torch.Tensor:
     is_debuffed = encode_bool(card.is_debuffed)
     is_face_down = encode_bool(card.is_face_down)
 
-    return torch.cat([rank, suit, enhancement, seal, edition, extra_chips, is_debuffed, is_face_down], dim=0)
+    features = torch.cat([rank, suit, enhancement, seal, edition, extra_chips, is_debuffed, is_face_down])
+    return features.unsqueeze(0)
 
 def all_subclasses(cls: Type) -> List[Type]:
     """
@@ -113,7 +116,7 @@ jokers_to_index = enum_to_index(all_jokers)
 
 def encode_jokers(jokers: list[BalatroJoker]) -> torch.FloatTensor:
     encoded = []
-    for i in range(0, max_jokers):
+    for i in range(0, MAX_JOKERS):
         if i < len(jokers):
             joker = jokers[i]
             type = one_hot(jokers_to_index[joker], len(all_jokers))
@@ -123,12 +126,13 @@ def encode_jokers(jokers: list[BalatroJoker]) -> torch.FloatTensor:
             is_rental = encode_bool(joker.is_rental)
             is_debuffed = encode_bool(joker.is_debuffed)
             is_flipped = encode_bool(joker.is_flipped)
-
             num_perishable_rounds_left = encode_int(joker.num_perishable_rounds_left)
-            encoded.append(torch.cat([type, edition, is_eternal, is_perishable, is_rental, is_debuffed, is_flipped, num_perishable_rounds_left]))
+
+            features = torch.cat([type, edition, is_eternal, is_perishable, is_rental, is_debuffed, is_flipped, num_perishable_rounds_left])
+            encoded.append(features.unsqueeze(0))
         else:
-            encoded.append(torch.zeros(len(all_jokers)+len(Edition)+5+1))
-    return torch.stack(encoded)
+            encoded.append(torch.zeros(1, len(all_jokers)+len(Edition)+5+1))
+    return torch.cat(encoded)
 
 
 # TODO: handle sellable
@@ -137,15 +141,15 @@ def encode(run: Run) -> torch.FloatTensor:
     ante_tags = encode_ante_tags(run.ante_tags)
 
     blind = encode_blind(run.blind)
-    blind_reward = torch.tensor([float(run.blind_reward)], dtype=torch.float32)
+    blind_reward = encode_int(run.blind_reward)
     boss_blind = encode_blind(run.boss_blind)
     consumable_slots = encode_int(run.consumable_slots)
     consumables = encode_consumables(run.consumables)
     if run.hand is None:
-        hand_cards = torch.zeros(max_hand_cards*(len(Rank)+len(Suit)+len(Enhancement)+len(Seal)+len(Edition)+3))
+        hand_cards = torch.zeros(MAX_HAND_CARDS, (len(Rank)+len(Suit)+len(Enhancement)+len(Seal)+len(Edition)+3))
     else:
-        hand_cards = encode_cards(run.hand, max_hand_cards)
-    deck_cards_left = encode_cards(run.deck_cards_left, max_deck_cards)
+        hand_cards = encode_cards(run.hand, MAX_HAND_CARDS)
+    deck_cards_left = encode_cards(run.deck_cards_left, MAX_DECK_CARDS)
     discards = encode_int(run.discards)
     if run.hands is None:
         hands = encode_int(0)
@@ -153,5 +157,7 @@ def encode(run: Run) -> torch.FloatTensor:
         hands = encode_int(run.hands)
     jokers = encode_jokers(run.jokers)
 
-    parts = [ante, ante_tags, blind, blind_reward, boss_blind, consumable_slots, consumables, hand_cards, deck_cards_left, discards, hands, jokers]
+    parts = [ante, ante_tags.view(-1), blind, blind_reward, boss_blind,
+        consumable_slots, consumables.view(-1), hand_cards.view(-1),
+        deck_cards_left.view(-1), discards, hands, jokers.view(-1)]
     return torch.cat(parts, dim=0)
